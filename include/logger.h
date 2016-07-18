0926723a975fb373487735d4602166cf70b9167a
smac89 <smac89@users.noreply.github.com>
2016-07-18 01:21:58 -0600

    Updated Cmake files to reflect new project structure

--- include/logger.h
+++ include/logger.h
@@ -1,5 +1,10 @@
+#ifndef OAUTH_LOGGER
+#define OAUTH_LOGGER
+
 void o_log(const char *message, ...);
 
 void e_log(const char *message, ...);
 
 void f_log(FILE *out, const char *message, ...);
+
+#endif
